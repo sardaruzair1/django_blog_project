b18744b138f3ffd9c6e11b6fa925c9041476a7eb
sardar uzair <sardaruzair123456789@gmail.com>
2022-12-19 03:02:27 +0500

    so compleated

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1@jq+n(a*#0!e-_m-cv0%xzz2$wwtk+eoc(j_m#m9o*4cvg+5u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','codewithsardar.herokuapp.com']
 
 
 # Application definition
